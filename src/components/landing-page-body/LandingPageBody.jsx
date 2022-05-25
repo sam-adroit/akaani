5ec7c7efd7726a9207c60a9be7279f4db7d7515e
unknown <samuelbankole779@gmail.com>
2022-05-25 11:30:37 +0100

    Made changes to basket links

--- src/components/landing-page-body/LandingPageBody.jsx
+++ src/components/landing-page-body/LandingPageBody.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 
 import "./LandingPageBody.css";
@@ -6,6 +6,7 @@ import CartBasket from "../cart-basket/CartBasket";
 import Card from "../card/Card";
 
 function LandingPageBody({ changeData, cardDetails }) {
+  const [active, setActive] = useState("Paid");
   return (
     <div className="Landing-page-body">
       <div className="landing-page-body-container">
@@ -29,15 +30,42 @@ function LandingPageBody({ changeData, cardDetails }) {
             <button>Create Basket</button>
           </div>
           <ul className="basket-link">
-            <li className="basket-link--item actives">Paid</li>
-            <li className="basket-link--item ">Pending</li>
-            <li className="basket-link--item">Completed</li>
+            <li
+              onClick={() => setActive("Paid")}
+              className={
+                active === "Paid"
+                  ? "basket-link--item actives"
+                  : "basket-link--item"
+              }
+            >
+              Paid
+            </li>
+            <li
+              onClick={() => setActive("Pending")}
+              className={
+                active === "Pending"
+                  ? "basket-link--item actives"
+                  : "basket-link--item"
+              }
+            >
+              Pending
+            </li>
+            <li
+              onClick={() => setActive("Completed")}
+              className={
+                active === "Completed"
+                  ? "basket-link--item actives"
+                  : "basket-link--item"
+              }
+            >
+              Completed
+            </li>
           </ul>
           <div className="basket-body">
             <span>
               <img src="./shop.png" alt="" />
             </span>
-            <p>No Paid Baskets, yet!</p>
+            <p>No {active} Baskets, yet!</p>
           </div>
         </div>
         <div className="basket-body-bottom" onClick={changeData}>
